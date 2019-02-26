83a63d9609b5b005d8ff55d4c6473c65a26f801a
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-26 22:41:47 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,9 @@
 // Code Product Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Product extends React.Component {
+  render() {
+    return ()
+  }
+}
