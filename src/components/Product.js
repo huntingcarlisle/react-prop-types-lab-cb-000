c98981eb7820f66881b87731f9c9a7b668599a7f
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-26 22:58:21 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -26,5 +26,25 @@ Product.propTypes = {
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
-  weight: PropTypes.number.isRequired
+  weight: weightCheck
 };
+
+function weightCheck(props, propName, componentName) {
+  componentName = componentName || 'ANONYMOUS';
+
+  const weight = props[propName];
+
+  if (weight === undefined) {
+    return new Error('The `weight` prop is required.');
+  }
+
+  if (isNaN(weight)) {
+    return new Error('The `weight` prop is not a number.');
+  }
+
+  const isValidWeight = weight > 80 && weight < 300;
+
+  if (!isValidWeight) {
+    return new Error('The `weight` prop should range between 80 and 300.');
+  }
+}
